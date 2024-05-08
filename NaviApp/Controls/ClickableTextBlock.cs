bfedeb00aed10c569dddec77fb1ea9c0b27c5911
alex.js <zhongwcool@163.com>
2024-05-08 16:34:07 +0800

    增加ClickableTextBlock

---
+++ NaviApp/Controls/ClickableTextBlock.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace NaviApp.Controls;
+
+public class ClickableTextBlock : Control
+{
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(nameof(Text), typeof(string), typeof(ClickableTextBlock),
+            new PropertyMetadata(default(string)));
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    // Click事件
+    public static readonly RoutedEvent ClickEvent =
+        EventManager.RegisterRoutedEvent(nameof(Click), RoutingStrategy.Bubble, typeof(RoutedEventHandler),
+            typeof(ClickableTextBlock));
+
+    // .NET事件包装器
+    public event RoutedEventHandler Click
+    {
+        add => AddHandler(ClickEvent, value);
+        remove => RemoveHandler(ClickEvent, value);
+    }
+
+    // 增加事件触发
+    protected virtual void OnClick()
+    {
+        var newEventArgs = new RoutedEventArgs(ClickEvent);
+        RaiseEvent(newEventArgs);
+    }
+
+    // 构造函数
+    static ClickableTextBlock()
+    {
+        // 通过覆盖元数据以实现按键调用OnClick方法
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(ClickableTextBlock),
+            new FrameworkPropertyMetadata(typeof(ClickableTextBlock)));
+    }
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        // 为整个控件添加点击事件处理程序
+        MouseLeftButtonDown += (s, e) =>
+        {
+            OnClick(); // 触发点击事件
+        };
+    }
+}
---
+++ NaviApp/Converters/BrushOpacityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace NaviApp.Converters;
+
+public class BrushOpacityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not SolidColorBrush brush) return null;
+        var opacity = System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+        SolidColorBrush rv = new(brush.Color)
+        {
+            Opacity = opacity
+        };
+        rv.Freeze();
+        return rv;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => Binding.DoNothing;
+}
